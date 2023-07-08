c1ec488729ea7a7d73ce3766c09262ce6f78d270
fjoralbash <fjoralba.shehaj@outlook.com>
2023-07-08 09:02:23 +0200

    modified:   contact.html modified:   index.html modified:   index.js modified:   main.css deleted:    menu.js

--- index.js
+++ index.js
@@ -20,5 +20,38 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-    
+
+
+
+
+// Contact subscribe
+
+function subscribe(event) {
+  event.preventDefault();
+  const email = document.querySelector("#home-section .home-section__mail-input").value;
+
+  if (email.includes("@") && email.includes(".")) {
+    document
+      .querySelector("#home-section .submitted-message")
+      .classList.add("succeed", "show-and-hide");
+    document.querySelector("#home-section .submitted-message p").innerHTML =
+      "Thank you! Your submission has been received!";
+  } else {
+    document
+      .querySelector("#home-section .submitted-message")
+      .classList.add("fail", "show-and-hide");
+    document.querySelector("#home-section .submitted-message p").innerHTML =
+      "Oops! Something went wrong while submitting the form!";
+  }
+
+  document.querySelector("#home-section .submitted-message").value = "";
+  setTimeout(() => {
+    document
+      .querySelector("#home-section .submitted-message p")
+      .classList.remove("show-and-hide", "succeed", "fail");
+  }, 5000);
+}
+
+
+
 
